9b6eb31d0cade68216e93644a73b71bda53a3df8
douglas <dougjuarez21.dev@gmail.com>
2025-08-01 18:27:28 -0600

    show place list with flat list

--- app/index.tsx
+++ app/index.tsx
@@ -1,15 +1,49 @@
-import { Text, View } from "react-native";
+import { FlatList, SafeAreaView, StyleSheet, View } from "react-native";
+
+import { PlaceCard } from "@/components/PlaceCard";
+import { places } from "@/data/data";
+import { colors } from "@/styles/colors";
+
+const Separator = () => {
+  return <View style={styles.separator} />;
+};
+
+const Footer = () => {
+  return <View />;
+};
+
+const Header = () => {
+  return <View style={styles.separator} />;
+};
 
 export default function Index() {
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
-    </View>
+    <SafeAreaView style={styles.container}>
+      <FlatList
+        style={styles.list}
+        data={places}
+        renderItem={({ item }) => <PlaceCard place={item} />}
+        keyExtractor={(item) => item.name}
+        ItemSeparatorComponent={Separator}
+        ListFooterComponent={Footer}
+        ListFooterComponentStyle={{ height: 25 }}
+        ListHeaderComponent={Header}
+        ListHeaderComponentStyle={{ height: 25 }}
+      />
+    </SafeAreaView>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: colors.White,
+  },
+  separator: {
+    height: 15,
+  },
+  list: {
+    flex: 1,
+    paddingHorizontal: 20,
+  },
+});

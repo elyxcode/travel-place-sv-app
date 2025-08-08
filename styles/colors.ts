1917869fa0bdba9fcc03ec9a32877f46c34b4c9f
douglas <dougjuarez21.dev@gmail.com>
2025-08-08 17:28:03 -0600

    colors added

--- styles/colors.ts
+++ styles/colors.ts
@@ -2,4 +2,7 @@ export const colors = {
     White: '#FFFFFF',
     Black: '#000000',
     TagColor: '#fefefe',
+    DescriptionColor: '#808080',
+    LocationTextColor: '#808080',
+    externalBorderButton: '#C4C4C4',
 }

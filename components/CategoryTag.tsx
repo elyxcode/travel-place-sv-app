d0fef6289ff4d7d1ebfb2d70dd895145f9405709
douglas <dougjuarez21.dev@gmail.com>
2025-08-08 17:39:19 -0600

    tag position prop

--- components/CategoryTag.tsx
+++ components/CategoryTag.tsx
@@ -4,18 +4,28 @@ import { StyleProp, StyleSheet, Text, View, ViewStyle } from "react-native";
 interface Props {
   category: string;
   extraStyles?: StyleProp<ViewStyle>;
+  positionOposition: boolean;
 }
 
-export const CategoryTag = ({ category, extraStyles }: Props) => {
+export const CategoryTag = ({
+  category,
+  extraStyles,
+  positionOposition = false,
+}: Props) => {
   return (
-    <View style={[styles.container, extraStyles]}>
+    <View
+      style={[
+        positionOposition ? styles.containerRight : styles.containerLeft,
+        extraStyles,
+      ]}
+    >
       <Text style={styles.text}>{category}</Text>
     </View>
   );
 };
 
 const styles = StyleSheet.create({
-  container: {
+  containerLeft: {
     position: "absolute",
     backgroundColor: colors.TagColor,
     borderRadius: 10,
@@ -23,6 +33,16 @@ const styles = StyleSheet.create({
     paddingVertical: 5,
     top: 10,
     left: 10,
+    zIndex: 9999,
+  },
+  containerRight: {
+    position: "absolute",
+    backgroundColor: colors.TagColor,
+    borderRadius: 10,
+    paddingHorizontal: 10,
+    paddingVertical: 5,
+    top: 10,
+    right: 10,
     zIndex: 9999,
   },
   text: {

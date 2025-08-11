738abf54d9a93033d3a64e3befd16f00868c6d0b
douglas <dougjuarez21.dev@gmail.com>
2025-08-11 16:02:20 -0600

    open external link function

--- components/ExternalLinkButton.tsx
+++ components/ExternalLinkButton.tsx
@@ -1,15 +1,27 @@
 import { colors } from "@/styles/colors";
 import EvilIcons from "@expo/vector-icons/EvilIcons";
-import React from "react";
-import { StyleSheet, Text, TouchableOpacity, View } from "react-native";
+import React, { useCallback } from "react";
+import {
+  Linking,
+  StyleSheet,
+  Text,
+  TouchableOpacity,
+  View,
+} from "react-native";
 
 interface Props {
-  uri?: string;
+  uri: string;
 }
 
 export const ExternalLinkButton = ({ uri }: Props) => {
+  const handlePress = useCallback(async () => {
+    const supported = await Linking.canOpenURL(uri);
+
+    if (supported) await Linking.openURL(uri);
+  }, [uri]);
+
   return (
-    <TouchableOpacity activeOpacity={0.5}>
+    <TouchableOpacity activeOpacity={0.5} onPress={handlePress}>
       <View style={styles.externalLinkButton}>
         <EvilIcons name="external-link" size={26} color="black" />
         <Text style={styles.externalLinkText}>

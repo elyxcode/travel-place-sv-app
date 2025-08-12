2004a6ea1216291be66b3c432bda5a0b2a385bf7
douglas <dougjuarez21.dev@gmail.com>
2025-08-12 15:57:30 -0600

    just splash screen hide function

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,5 +1,11 @@
 import { Stack } from "expo-router";
+import * as SplashScreen from "expo-splash-screen";
+import { useEffect } from "react";
 
 export default function RootLayout() {
+  useEffect(() => {
+    SplashScreen.hide();
+  });
+
   return <Stack screenOptions={{ headerShown: false }} />;
 }

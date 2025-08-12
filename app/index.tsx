96db185c10f8006b15ec842e80a6b0651f1b5d05
douglas <dougjuarez21.dev@gmail.com>
2025-08-12 17:47:40 -0600

    use state managment

--- app/index.tsx
+++ app/index.tsx
@@ -3,7 +3,10 @@ import { FlatList, SafeAreaView, StyleSheet, View } from "react-native";
 import { HeaderList } from "@/components/HeaderList";
 import { PlaceCard } from "@/components/PlaceCard";
 import { places } from "@/data/data";
+import { useStore } from "@/store";
 import { colors } from "@/styles/colors";
+import { useEffect, useState } from "react";
+import LoadingComponent from "../components/LoadingComponent";
 
 const Separator = () => {
   return <View style={styles.separator} />;
@@ -14,11 +17,24 @@ const Footer = () => {
 };
 
 export default function Index() {
+  const { Places, AddPlaces } = useStore();
+  const [isLoading, setIsLoading] = useState(false);
+
+  useEffect(() => {
+    setIsLoading(true);
+
+    AddPlaces(places);
+
+    setIsLoading(false);
+  }, [isLoading, AddPlaces]);
+
+  if (isLoading) return <LoadingComponent />;
+
   return (
     <SafeAreaView style={styles.container}>
       <FlatList
         style={styles.list}
-        data={places}
+        data={Places}
         renderItem={({ item }) => <PlaceCard place={item} />}
         keyExtractor={(item) => item.name}
         ItemSeparatorComponent={Separator}

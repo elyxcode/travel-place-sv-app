5dcf41ef6a413ab51eb4427f9ec2bbe127f93e5e
douglas <dougjuarez21.dev@gmail.com>
2025-09-08 10:12:31 -0600

    add privacy and user terms buttons

--- app/settings.tsx
+++ app/settings.tsx
@@ -1,12 +1,16 @@
 import { BackButton } from "@/components/BackButton";
+import { ExternalLinkButton } from "@/components/ExternalLinkButton";
 import React from "react";
-import { SafeAreaView, StyleSheet, Text } from "react-native";
+import { SafeAreaView, StyleSheet, View } from "react-native";
 
 export default function Settings() {
   return (
     <SafeAreaView style={styles.container}>
       <BackButton />
-      <Text>Settings</Text>
+      <View style={styles.containerLink}>
+        <ExternalLinkButton uri="" name="Terminos y condiciones" />
+        <ExternalLinkButton uri="" name="Terminos de privacidad" />
+      </View>
     </SafeAreaView>
   );
 }
@@ -16,6 +20,9 @@ const styles = StyleSheet.create({
     flex: 1,
     marginTop: 20,
     justifyContent: "center",
-    alignItems: "center",
+  },
+  containerLink: {
+    gap: 10,
+    paddingHorizontal: 30,
   },
 });
